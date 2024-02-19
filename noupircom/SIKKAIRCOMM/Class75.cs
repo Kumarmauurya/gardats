a246964332ccbd427bd09c9f7fca1be2e38a727c
gadarts <117938694+Kumarmauurya@users.noreply.github.com>
2024-02-19 21:10:21 +0530

    up

--- noupircom/SIKKAIRCOMM/Class75.cs
+++
@@ -1,6 +0,0 @@
-internal sealed class Class75
-{
-	internal Class74 class74_0;
-
-	internal Class74 class74_1;
-}
--- noupircom/SIKKAIRCOMM/Struct5.cs
+++
@@ -1,6 +0,0 @@
-internal struct Struct5
-{
-	public int int_0;
-
-	public int int_1;
-}
